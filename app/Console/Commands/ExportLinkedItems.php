a417cd036537343a1b9df78056abaa84c7f0221e
Kevin Op den Kamp <k.opdenkamp@gmail.com>
2022-09-25 17:20:34 +0200

    Added missing item links and manual changes that are not present in the TMB tables

--- app/Console/Commands/ExportLinkedItems.php
+++ app/Console/Commands/ExportLinkedItems.php
@@ -40,7 +40,101 @@ class ExportLinkedItems extends Command
 				];
 			}
 
+			// PREPEND
+			if ($item->item_id === 19366) {
+				$itemLinks[$item->parentItem->item_id]['children'][19003] = (object) [
+					'item_id' => 19003,
+					'name' => 'Head of Nefarian (Alliance)',
+				];
+			}
+			
 			$itemLinks[$item->parentItem->item_id]['children'][$item->item_id] = $item;
+
+			// APPEND
+			if ($item->item_id === 18406) {
+				$itemLinks[$item->parentItem->item_id]['children'][18423] = (object) [
+					'item_id' => 18423,
+					'name' => 'Head of Onyxia (Alliance)',
+				];
+
+				$itemLinks[18423] = [
+					'name' => 'Head of Onyxia (Alliance)',
+					'children' => [
+						18403 => (object) [
+							'item_id' => 18403,
+							'name' => 'Dragonslayer\'s Signet',
+						],
+						18404 => (object) [
+							'item_id' => 18404,
+							'name' => 'Onyxia Tooth Pendant',
+						],
+						18406 => (object) [
+							'item_id' => 18406,
+							'name' => 'Onyxia Blood Talisman',
+						],
+						18422 => (object) [
+							'item_id' => 18422,
+							'name' => 'Head of Onyxia (Horde)',
+						],
+					],
+				];
+			} else if ($item->item_id === 19384) {
+				$itemLinks[19003] = [
+					'name' => 'Head of Nefarian (Alliance)',
+					'children' => [
+						19002 => (object) [
+							'item_id' => 19002,
+							'name' => 'Head of Nefarian (Horde)',
+						],
+						19366 => (object) [
+							'item_id' => 19366,
+							'name' => 'Master Dragonslayer\'s Orb',
+						],
+						19383 => (object) [
+							'item_id' => 19383,
+							'name' => 'Master Dragonslayer\'s Medallion',
+						],
+						19384 => (object) [
+							'item_id' => 19384,
+							'name' => 'Master Dragonslayer\'s Ring',
+						],
+					],
+				];
+			} else if ($item->item_id === 28793) {
+				$itemLinks[$item->parentItem->item_id]['children'][32386] = (object) [
+					'item_id' => 32386,
+					'name' => 'Magtheridon\'s Head (Horde)',
+				];
+
+				$itemLinks[32386] = [
+					'name' => 'Magtheridon\'s Head (Horde)',
+					'children' => [
+						28790 => (object) [
+							'item_id' => 28790,
+							'name' => 'Naaru Lightwarden\'s Band',
+						],
+						28791 => (object) [
+							'item_id' => 28791,
+							'name' => 'Ring of the Recalcitrant',
+						],
+						28792 => (object) [
+							'item_id' => 28792,
+							'name' => 'A\'dal\'s Signet of Defense',
+						],
+						28793 => (object) [
+							'item_id' => 28793,
+							'name' => 'Band of Crimson Fury',
+						],
+						32385 => (object) [
+							'item_id' => 32385,
+							'name' => 'Magtheridon\'s Head (Alliance)',
+						],
+					],
+				];
+
+				// Add the faction to magtheridon's head
+				$itemLinks[32385]['name'] = 'Magtheridon\'s Head (Alliance)';
+			}
 		}
 
 		$lastParentItemId = null;
@@ -48,14 +142,14 @@ class ExportLinkedItems extends Command
 		foreach ($itemLinks as $parentItemId => $details) {
 			if ($lastParentItemId != $parentItemId) {
 				$lastParentItemId = $parentItemId;
-				$exportString .= sprintf("\n},\n-- %s\n[\"%s\"] = {", $details['name'], $parentItemId);
+				$exportString .= sprintf("\n    },\n    -- %s\n    [\"%s\"] = {", $details['name'], $parentItemId);
 			}
 
 			foreach ($details['children'] as $child) {
-				$exportString .= sprintf("\n\t%s, -- %s", $child->item_id, $child->name);
+				$exportString .= sprintf("\n        %s, -- %s", $child->item_id, $child->name);
 			}
 		}
-		$exportString .= "\n},";
+		$exportString .= "\n    },";
 		
 		// Remove the first two lines
 		$exportString = substr($exportString, strpos($exportString, "\n", strpos($exportString, "\n") + 1) + 1);
